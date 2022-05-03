483ac7900e6cf1ba1e1afc8c516fcd1785e52d87
nairo-mudumane <nairomudumane@outlook.com>
2022-05-03 15:03:21 +0200

    installed headlessui

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from "@headlessui/react";
 
 export function Widget() {
   return (
-    <div>
-      <ChatTeardropDots size={100} weight="thin" />
-    </div>
+    <Popover className="absolute bottom-5 right-6">
+      <Popover.Panel>ola</Popover.Panel>
+
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500">
+          <span className="pl-2" />
+          feedback
+        </span>
+      </Popover.Button>
+    </Popover>
   );
 }
